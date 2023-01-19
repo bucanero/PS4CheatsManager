630c2b73453240579983ecc93cea6649fd330dfc
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-01-19 06:41:30 -0600

    Add check for local PKG installation (#81)
    Co-authored-by: Damián Parrino <bucanero@users.noreply.github.com>

--- source/settings.c
+++ source/settings.c
@@ -158,8 +158,9 @@ void update_callback(int sel)
 
 	if (show_dialog(1, "New version available! Download update?"))
 	{
-		if (http_download(url->valuestring, "", "/data/goldcheats.pkg", 1))
-			show_message("Update downloaded to /data/goldcheats.pkg");
+		char* download_path = (dir_exists("/data/pkg/") == SUCCESS) ? "/data/pkg/goldcheats.pkg" : "/data/goldcheats.pkg";
+		if (http_download(url->valuestring, "", download_path, 1))
+			show_message("Update downloaded to %s", download_path);
 		else
 			show_message("Download error!");
 	}
