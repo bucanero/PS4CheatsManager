82daadbc665305fc2766ffb426e00982b32f4c07
Damian Parrino <bucanero@users.noreply.github.com>
2023-09-20 15:35:56 -0300

    Update version

--- include/settings.h
+++ include/settings.h
@@ -1,4 +1,4 @@
-#define GOLDCHEATS_VERSION          "1.1.0"     //GoldCheats PS4 version (about menu)
+#define GOLDCHEATS_VERSION          "1.1.4"     //GoldCheats PS4 version (about menu)
 
 #define MENU_TITLE_OFF			45			//Offset of menu title text from menu mini icon
 #define MENU_ICON_OFF 			105         //X Offset to start printing menu mini icon
